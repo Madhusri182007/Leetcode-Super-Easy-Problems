0ba8b91a30817220471d7fd5b1ea9cb18edb8d7a
Madhusri182007 <madhusri182007@gmail.com>
2025-03-08 20:53:47 +0530

    Create 1550_Three_consecutive_odds.c

---
+++ 1550_Three_consecutive_odds.c
@@ -0,0 +1,17 @@
+bool threeConsecutiveOdds(int* arr, int arrSize)
+{
+    if(arrSize<3)
+    {
+        return false;
+    }
+    int c=0;
+    for(int i=2;i<arrSize;i++)
+    {
+       
+            if(arr[i]%2==1 && arr[i-1]%2==1 && arr[i-2]%2==1)
+            {
+                return true;
+            }
+    }
+    return false;
+}
